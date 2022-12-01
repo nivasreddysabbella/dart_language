c384cf2fd2b26dc7f9302b34e8c43e381340a6c7
nivasreddysabbella <69564538+nivasreddysabbella@users.noreply.github.com>
2022-12-01 11:42:33 +0530

    comments in dart

---
+++ comments.dart
@@ -0,0 +1,16 @@
+void main(){
+  print("nivas is a good boy");
+  print(6789);
+  // this a single line comment
+}
+/*
+ * this is a multiline comment
+ * and this is occured here
+*/
+// document comment for better understanding of the function
+/// this is used for creating a new function
+void nii(){
+  //sting
+}
+
+// when we click on the nii() fuction the documentation will show the triple forword slash "matter"
