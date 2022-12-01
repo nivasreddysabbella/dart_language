0c2c46a64a85f868ba483f00ccde49b6b41ae45e
nivasreddysabbella <69564538+nivasreddysabbella@users.noreply.github.com>
2022-12-01 11:32:45 +0530

    helloworld

---
+++ helloworld.dart
@@ -0,0 +1,9 @@
+void main()//it is like a start of the application
+{
+  print("hello world"); // we can directly print the string using the double quotes
+  print("this is nivas");
+  
+  print(3443); //without any quotes directly print the numbers
+  
+  print(23+34);// directly we can do the arthimetic operations like +-*/ any of these
+}
