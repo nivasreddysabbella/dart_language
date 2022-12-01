56ee95520b3d7857dbb8cb7c572bc768edeb5a8e
nivasreddysabbella <69564538+nivasreddysabbella@users.noreply.github.com>
2022-12-01 12:03:28 +0530

    data types and implemention

---
+++ datatypes.dart
@@ -0,0 +1,37 @@
+/*
+ * number
+ * --int
+ * --double(fraction part)
+ * string(single character also string)--String(representation)
+ * Boolean(bool)--default "null"
+ * list
+ * maps
+ * runes(for expressing unicode expressions in strings)
+ * symbols
+*/
+
+void main(){
+  // number: int
+  int age=10;
+  
+  // numbers: double
+  double price=10.50;
+  
+  //strings
+  
+  String name="sai baba";
+  
+  //boolean
+  
+  bool alive=true;
+  bool dead=false;
+  bool ne;
+  
+  
+  // or we can directly use the var keyword
+  // it will decide own
+  
+  var aage=10;
+  print(age);
+  
+}
