75aaf83a8f3cab397168a2e24724266ab72ef614
nivasreddysabbella <69564538+nivasreddysabbella@users.noreply.github.com>
2022-12-08 12:56:24 +0530

    these are the switch statements

---
+++ switch_statements.dart
@@ -0,0 +1,14 @@
+void main() {
+  var grade="A";
+  switch(grade){
+    case 'A':
+      print("excellent");
+      break;
+    case 'B':
+      print("good");
+      break;
+    default:
+      print("need to improve a lot");
+      break;
+  }
+}
