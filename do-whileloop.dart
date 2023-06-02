171d25a2dd48d1bce9c0aa1598b18f985a267938
nivasreddysabbella <69564538+nivasreddysabbella@users.noreply.github.com>
2023-06-02 12:43:52 +0530

    Create do-whileloop.dart

---
+++ do-whileloop.dart
@@ -0,0 +1,34 @@
+void main() {
+  // use of do-while loop
+  /*   var count =1;
+  while (count<=10){
+    print(count);
+    count=count+1;//count++;
+  }
+  
+  var count =1;
+  do{
+    print(count);
+    count++;
+    
+  }while(count<=10);// ending need to have the colon
+  */
+  
+  
+  var count =1;
+  do{
+    if (count%2==0){
+      print(count);}
+    count++;
+    
+  }while(count<=10);// ending need to have the colon
+}
+
+/*the output for the code is:
+ * 2
+4
+6
+8
+10
+*/
+
