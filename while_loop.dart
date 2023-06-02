ac028be22c72b47b9279efc78b9aae7f158c3ee9
nivasreddysabbella <69564538+nivasreddysabbella@users.noreply.github.com>
2023-06-02 12:37:00 +0530

    Create while_loop.dart

---
+++ while_loop.dart
@@ -0,0 +1,33 @@
+void main() {
+  var count =1;
+  while (count<=10){
+    print(count);
+    count=count+1;//count++;
+  }
+  // for printing even numbers
+  var cou=1;
+  while (cou<=10){
+    if (cou%2==0){
+    print(cou);
+    }
+    cou=cou+1;//cou++;
+  }
+}
+
+/* the ouput for the above code is:
+ * 1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+2
+4
+6
+8
+10
+*/
